7b2fd29598122dda7e422a7a5842acd957f48df2
jpeer264 <jan.oster94@gmail.com>
2016-02-22 08:35:49 +0100

    project setup

---
+++ config/grunt/management.js
@@ -0,0 +1,324 @@
+/*
+ * @start Managing Files
+ *
+ * @prod
+ *
+ * @used plugins
+ *   ** concat   <- merges all files together
+ *   ** sass     <- convert from `scss` to `css`
+ *   ** clean    <- delete folder and files
+ *   ** copy     <- copy files from one dir to another
+ *   ** cdnify   <- rewrite HTML files from *.css to *.min.css or *.js
+ *   ** html2js  <- 4 angularJS. Make own module for all components
+ */
+module.exports = {
+	concat: {
+		// concat all js files in assets, excluding possible vendor files
+		js: {
+			options: {
+				process: function(src, filepath) {
+					// TODO replace use strict
+					var filename = filepath.split('/').pop();
+					var wrapper;
+
+					if (filename.indexOf('app') > 0) {
+						wrapper = 	(filepath.indexOf('_intro') === -1 ? ";(function( window, jQuery, angular, undefined ){ \n 'use strict';\n\n":"") +
+										src +
+									(filepath.indexOf('_intro') === -1 ? "\n\n}( window, jQuery, angular ));":"");
+					} else {
+						wrapper = src;
+					}
+
+			  		return "// Source: " + filepath + "\n" +
+			  				wrapper
+				},
+			},
+			files: [
+				{
+					src: [
+						'<%= paths.src.files.assets.js %>',
+						'<%= paths.src.ignore.couldBeVendor %>'
+					],
+					dest: '<%= paths.tmp.folder.assets.js %>/main.js'
+				}
+			]
+		},
+
+		// concat all vendor files
+		vendor: {
+			options: {
+				banner: 	'window.jQuery = {};\n' +
+							'window.angular = {};\n' +
+							'window.module = {};\n'
+			},
+			files: [{
+				src: [
+					'<%= paths.tmp.folder.assets.js %>/bower.js',
+					'<%= paths.src.files.couldBeVendor.js %>',
+					'<%= paths.vendor.js %>',
+					'<%= paths.src.ignore.min %>'
+				],
+				dest: '<%= paths.tmp.folder.assets.js %>/vendor.js'
+			}]
+		},
+
+		// concat all angular files
+		app: {
+			options: {
+				process: function(src, filepath) {
+					return "// Source: " + filepath + "\n" +
+			  				(filepath.indexOf('_intro') === -1 ? ";(function( window, jQuery, angular, undefined ){ \n 'use strict';\n\n":"") +
+								src +
+							(filepath.indexOf('_intro') === -1 ? "\n\n}( window, jQuery, angular ));":"");
+				}
+			},
+			files: [{
+				src: [
+					'<%= paths.src.files.angular.all %>',
+					'<%= paths.tmp.folder.assets.js %>/template.js'
+				],
+				dest: '<%=paths.tmp.folder.assets.js %>/app.js'
+			}]
+		},
+
+		// concat all tests and instrumented files
+		tests: {
+			files: {
+				'<%= paths.tmp.folder.tests.base %>/js/tests.js': '<%= paths.src.tests %>',
+				'<%= paths.tmp.folder.tests.js %>/instrument.js': '<%= paths.tmp.files.instrumented %>',
+				'<%= paths.tmp.folder.tests.js %>/vendor.js' : [
+					'<%= paths.tmp.folder.assets.js %>/bower.js',
+					'<%= paths.src.files.couldBeVendor.js %>',
+					'<%= paths.vendor.js %>',
+					'<%= paths.src.ignore.min %>'
+				]
+			}
+		},
+
+		// concat all scss files
+		scss: {
+			options: {
+				// add the sourcefile as comment
+				process: function(src, filepath) {
+					return '/* Source: ' + filepath + '*/\n' + src
+				}
+			},
+			files: [
+				{
+					src: '<%= paths.src.files.scss %>',
+					dest: '<%= paths.src.folder.assets.scss %>/createdFileByGrunt.scss'
+				}
+			]
+		},
+
+		// concat all css files
+		css: {
+			options: {
+				// add the sourcefile as comment
+				process: function(src, filepath) {
+					return '/* Source: ' + filepath + '*/\n' + src
+				}
+			},
+			files: [{
+					src: [
+						'<%= paths.vendor.css %>',
+						'<%= paths.tmp.folder.assets.css %>/SassFiles.css',
+						'<%= paths.src.files.css %>',
+					],
+					dest: '<%= paths.tmp.folder.assets.css %>/global.css'
+			}]
+		},
+	},
+
+	bower_concat: {
+		all: {
+			dest: '<%= paths.tmp.folder.assets.js %>/bower.js',
+			bowerOptions: {
+			// relative: false
+			},
+			dependencies: {
+				'angular': 'jquery'
+			},
+			process: function(src) {
+				"\n" +
+				";(function( window, jQuery, angular, undefined ){ \n 'use strict';\n\n" +
+					src +
+				"\n\n}( window, jQuery, angular ));"
+			} ,
+			callback: function(file, comp) {
+				return file;
+			}
+		}
+	},
+
+	sass: {
+		dev: {
+			src: '<%=  paths.src.folder.assets.scss %>/createdFileByGrunt.scss',
+			dest: '<%= paths.tmp.folder.assets.css %>/SassFiles.css'
+			// '<%=paths.tmp.css%>/global.css' : '<%=paths.tmp.scss%>/build.scss'
+		}
+	},
+
+	clean: {
+		tmp: {
+			src: '<%= paths.tmp.base %>'
+		},
+
+		dist: {
+			src: '<%= paths.dest.base %>'
+		},
+
+		cache: {
+			src: '<%= paths.cache.scss %>'
+		},
+
+		createdSassByGrunt: {
+			src: [
+				'<%= paths.src.folder.assets.scss %>/createdFileByGrunt.scss',
+				'<%= paths.tmp.folder.assets.css %>/SassFiles.css',
+			]
+		},
+		bower: {
+			src: '<%= paths.tmp.folder.assets.js %>/bower.js'
+		},
+		template: {
+			src: '<%= paths.tmp.folder.assets.js %>/template.js'
+		}
+	},
+
+	copy: {
+		dev: {
+			files: [
+				// copy all images, svgs and jsons from assets
+				{
+					expand: true,
+					cwd: '<%= paths.src.folder.assets.base %>',
+					src: ['img/**', 'svg/**', 'json/**'],
+					dest: '<%= paths.tmp.folder.assets.base %>'
+				},
+
+				// copy index.html file
+				{
+					expand: true,
+					cwd: '<%= paths.src.base %>',
+					src: [
+						'**/*.{html,php}',
+						'!**/app/**/*.html',
+					],
+					dest: '<%= paths.tmp.base %>/'
+				}
+			]
+		},
+
+		prod: {
+			files: [
+				// copy all images, svgs and jsons from assets
+				{
+					expand: true,
+					cwd: '<%= paths.src.folder.assets.base %>',
+					src: ['img/**', 'svg/**', 'json/**'],
+					dest: '<%= paths.dest.folder.assets.base %>'
+				},
+
+				// copy index.html file
+				{
+					expand: true,
+					cwd: '<%= paths.src.base %>',
+					src: [
+						'**/*.{html,php}',
+						'!**/app/**/*.html',
+					],
+					dest: '<%= paths.dest.base %>'
+				}
+			]
+		},
+
+		cache_sass: {
+			files: [{
+				expand: true,
+				cwd: '<%= paths.src.base %>',
+				src: '**/*.scss',
+				dest: '<%= paths.cache.sass %>'
+			}]
+		},
+
+		vendor: {
+			files: [{
+				expand: true,
+				cwd: '<%= paths.tmp.folder.assets.js %>',
+				src: 'vendor.js',
+				dest: '<%= paths.dest.folder.assets.js %>',
+				ext: '.min.js'
+			}]
+		},
+
+		tests: {
+			files: [{
+				expand: true,
+				cwd: './config/test',
+				src: [
+					'**'
+				],
+				dest: '<%= paths.tmp.folder.tests.base %>'
+			}]
+		}
+	},
+
+	cdnify: {
+		prod: {
+			options: {
+				rewriter: function (url) {
+
+					var arr = url.split('.');
+					if (arr[arr.length - 2] !== 'min') {
+						arr.splice((arr.length - 1), 0, 'min');
+					}
+					return arr.join('.');
+				},
+				css: false,
+				html: {
+					'img[src]': false,
+					'video[poster]': false,
+					'source[src]': false
+				}
+			},
+			files: [{
+				expand: true,
+				cwd: '<%= paths.dest.base %>',
+				src: ['**/*.{php,html}'],
+				dest: '<%= paths.dest.base %>'
+			}],
+		}
+	},
+
+	html2js: {
+		options: {
+			htmlmin: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeAttributeQuotes: true,
+				removeEmptyAttributes: true,
+				collapseBooleanAttributes: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+			},
+			module: '<%= pkg.name %>.templates',
+			singleModule: true,
+		},
+		template: {
+			src:  '<%= paths.src.files.angular.html %>',
+			dest: '<%= paths.tmp.folder.assets.js %>/template.js',
+		},
+	},
+
+	jsdoc: {
+		documentation: {
+			src: [
+				'<%= paths.src.allFiles.js %>',
+				'<%= paths.src.ignore.couldBeVendor %>',
+			],
+			dest: '<%= paths.tmp.folder.docs.base %>'
+		}
+	}
+};
---
+++ src/assets/js/app.js
@@ -0,0 +1,43 @@
+angular.module('railroad', [
+    'ui.router',
+    'LocalStorageModule',
+    'railroad.templates',
+    'pages',
+    'service',
+    'cmps'
+]);
+angular
+    .module('railroad')
+    .config([
+        '$stateProvider',
+        '$locationProvider',
+        '$urlRouterProvider',
+        'localStorageServiceProvider',
+        function($stateProvider, $locationProvider, $urlRouterProvider, localStorageServiceProvider) {
+            //redirect to home state when we call the page without route information
+            // $locationProvider.html5Mode(true);
+
+            $urlRouterProvider.when('', '/');
+            $urlRouterProvider.otherwise('/');
+
+            $stateProvider
+                .state('index', {
+                    url: '/',
+                    views: {
+                        main: {
+                            templateUrl: '',
+                            controller: ''
+                        }
+                    }
+                });
+
+            localStorageServiceProvider
+                .setPrefix('railroad')
+                .setStorageType('localStorage');
+        },
+
+    ]);
+
+angular.element(document).ready(function() {
+    angular.bootstrap(document, ['railroad']);
+});
