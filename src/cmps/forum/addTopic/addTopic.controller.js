1ab6d4f0ea052c3457fcd6e219b0514cdbb985fb
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-05-14 00:45:04 +0200

    add topic restricted privacy options

--- src/cmps/forum/addTopic/addTopic.controller.js
+++ src/cmps/forum/addTopic/addTopic.controller.js
@@ -23,15 +23,82 @@ function AddTopicController($scope, job, company) {
         event.preventDefault();
     });
 
-    $('button').on('click', function() {
-  $('#addTopic').foundation('reveal','open');
-});
+    $scope.listJob=[];
+    $scope.listCompany=[];
+    $scope.everyone=false;
+    $scope.jobsVisible=[];
+    $scope.companiesVisible=[];
+
 
     job.getAll().then(function(data) {
         $scope.jobs = data.plain();
+
+            company.getAll().then(function(data) {
+                $scope.companies = data.plain();
+                if($scope.currentUser.role_id==1||$scope.currentUser.role_id==2){
+                    $scope.jobsVisible=$scope.jobs;
+                    $scope.companiesVisible=$scope.companies;
+                }else if($scope.currentUser.role_id==3){
+                    $scope.jobsVisible.push($scope.currentUser.job);
+                    $scope.jobsVisible.push($scope.currentUser.job);
+                    //$scope.companiesVisible=$scope.currentUser.company;
+                }else {
+                    $scope.jobsVisible.push($scope.currentUser.job);
+                    //$scope.companiesVisible=$scope.currentUser.company;
+                }
+              console.log($scope.jobsVisible);
+            });
+
     });
 
-    company.getAll().then(function(data) {
-        $scope.companies = data.plain();
-    });
+
+
+
+
+      $scope.toggleJob = function (job, list) {
+
+        var idx = $scope.listJob.indexOf(job);
+            if (idx > -1) {
+              $scope.listJob.splice(idx, 1);
+            }
+            else {
+              $scope.listJob.push(job);
+            }
+            console.log($scope.listJob);
+        };
+
+      $scope.toggleCompany = function (company, list) {
+        var idx = $scope.listCompany.indexOf(company);
+            if (idx > -1) {
+              $scope.listCompany.splice(idx, 1);
+            }
+            else {
+              $scope.listCompany.push(company);
+            }
+            console.log($scope.listCompany);
+
+        };
+
+        $scope.checkAll = function () {
+            if($scope.everyone){
+                $scope.everyone=false;
+            }else{
+                $scope.everyone=true;
+            }
+
+            console.log($scope.everyone);
+         };
+
+
+
+         /*    angular.forEach($scope.jobs, function(job, key) {
+                   $scope.listJob.push(job);
+                 });
+                 console.log( $scope.listJob);
+
+                 angular.forEach($scope.companies, function(company, key) {
+                       $scope.listCompany.push(company);
+                     });
+                     console.log( $scope.listCompany);
+                     */
 }
