4e5a2c80419ca09ce691db69ac57d8edcb961c9a
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-05-28 00:46:59 +0200

    signup page

--- src/cmps/page/signUp/signUp.controller.js
+++ src/cmps/page/signUp/signUp.controller.js
@@ -7,14 +7,41 @@ angular
  * @name cmps.page:SignUpCtrl
  *
  * @requires $scope
+ * @requires $location
+ * @requires job
+ * @requires company
+ * @requires user
  *
  * @description
  * SignUpCtrl for the signUp directive
  */
 SignUpController.$inject = [
-    '$scope'
+    '$scope',
+    '$location',
+    'job',
+    'company',
+    'user'
 ];
 
-function SignUpController($scope) {
+function SignUpController($scope, $location, job, company, user) {
 
+    job.getAll().then(function(data) {
+        $scope.jobs = data.plain();
+        console.log($scope.jobs);
+    });
+
+    company.getAll().then(function(data) {
+        $scope.companies = data.plain();
+        console.log($scope.companies);
+    });
+
+    $scope.signup= function(){
+        user.create($scope.user).then( function (data){
+            console.log("user created");
+            console.log(data.plain());
+        });
+    }
+    $scope.back = function(){
+        $location.path( '/welcome' );
+    }
 }
