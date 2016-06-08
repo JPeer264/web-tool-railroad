54bdb36c75dcb215681b8b4f97c8763686ea8a4c
Jan Peer <jan@Jans-MacBook-Pro.local>
2016-06-08 20:08:39 +0200

    Added menu text to btn and deleted logs

--- src/cmps/page/news/news.directive.js
+++ src/cmps/page/news/news.directive.js
@@ -10,26 +10,26 @@ angular
  * Generates a single news component
  */
 function news() {
-        // Runs during compile
-        return {
-            // name: '',
-            // priority: 1,
-            // terminal: true,
-            scope: {
-                'header': '@newsHeader',
-                'content': '@newsContent',
-                'url': '@newsHref'
-            }, // {} = isolate, true = child, false/undefined = no change
-            controller: 'NewsCtrl',
-            // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-            // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-            // template: '',
-            templateUrl: 'cmps/page/news/news.html',
-            replace: true,
-            // transclude: true,
-            // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-            link: function($scope, iElm, iAttrs, controller) {
-                console.log(iAttrs);
-            }
-        };
+    // Runs during compile
+    return {
+        // name: '',
+        // priority: 1,
+        // terminal: true,
+        scope: {
+            'header': '@newsHeader',
+            'content': '@newsContent',
+            'url': '@newsHref'
+        }, // {} = isolate, true = child, false/undefined = no change
+        controller: 'NewsCtrl',
+        // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+        // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+        // template: '',
+        templateUrl: 'cmps/page/news/news.html',
+        replace: true,
+        // transclude: true,
+        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+        link: function($scope, iElm, iAttrs, controller) {
+
+        }
+    };
 };
