aba3fa6b21086b5306f5e1be953dfcb51a18e85c
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-06-15 17:25:48 +0200

    changed layout for files in comment

--- src/cmps/forum/comment/comment.controller.js
+++ src/cmps/forum/comment/comment.controller.js
@@ -16,5 +16,13 @@ CommentController.$inject = [
 ];
 
 function CommentController($scope) {
-
+    angular.forEach($scope.comment.file, function (file, key) {
+             if(file.filename.length >= 30){
+                $scope.split = file.filename.split('.');
+                $scope.name = $scope.split[0].slice(0, 30);
+                $scope.extension = $scope.split[1];
+                console.log($scope.name, $scope.extension);
+                file.filename = $scope.name + "..." + $scope.extension;
+             }
+        });
 }
