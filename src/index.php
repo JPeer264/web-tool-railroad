aef3eba41895ba6fcef9be89b347fc21c663c0dc
JPeer264 <jan.oster94@gmail.com>
2016-10-11 16:36:36 +0200

    updated paths

--- src/index.php
+++ src/index.php
@@ -4,9 +4,9 @@
         <title>WeToVi</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="/assets/css/global.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/global.css">
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600|Raleway:800,500' rel='stylesheet' type='text/css'>
-        <base href="/">
+        <base href="">
     </head>
     <body>
         <div class="railroad-body">
@@ -18,9 +18,9 @@
             <div data-ui-view="footer"></div>
         </div>
 
-        <script type="text/javascript" src="/assets/js/vendor.js"></script>
-        <script type="text/javascript" src="/assets/js/app.js"></script>
-        <script type="text/javascript" src="/assets/js/main.js"></script>
+        <script type="text/javascript" src="assets/js/vendor.js"></script>
+        <script type="text/javascript" src="assets/js/app.js"></script>
+        <script type="text/javascript" src="assets/js/main.js"></script>
         <script>
 
             jQuery(document).ready(function($) {
